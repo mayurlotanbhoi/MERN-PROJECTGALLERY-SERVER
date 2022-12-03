54c79eee05e8f6ff1997f80e500debe4db786ff4
mayur bhoi <mayurbhoi200@gmail.com>
2022-12-03 09:01:16 +0530

    16

--- Routers/logout.js
+++ Routers/logout.js
@@ -1,17 +1,28 @@
 const Rout = require("express").Router();
 
 Rout.get("/logout", (req, res) => {
-  
-      res.status(200)
+
+  try {
+
+    console.log(req)
+
+    res.status(200)
       .cookie("jwtoken", "", {
         httpOnly: true,
-        expires: Date.now(),
+        expires: Date.now(0),
         secure: true,
         sameSite: "None",
-       
-        path: "/"
+        path: "/",
+        overwrite: true,
       })
       .json({ massege: "Logout SuccesFull" });
+    
+  } catch (error) {
+    console.log(error)
+    
+  }
+  
+      
 });
 
 module.exports = Rout;
