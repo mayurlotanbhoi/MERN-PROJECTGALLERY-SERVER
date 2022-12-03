c2ec2c913363389f4f107856da5f2cbd1d6e1594
mayur bhoi <mayurbhoi200@gmail.com>
2022-12-03 08:41:07 +0530

    8

--- Routers/logout.js
+++ Routers/logout.js
@@ -5,7 +5,7 @@ Rout.get("/logout", (req, res) => {
       res.status(200)
       .cookie("jwtoken", "no data", {
         httpOnly: true,
-        expires: new Date.now(),
+        expires: new Date(Date.now()),
         secure: true,
         sameSite: "None",
        
