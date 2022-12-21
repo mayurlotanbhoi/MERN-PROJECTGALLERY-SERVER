255297a3e038f5946af37ecd2438bfd8abc50ef5
mayur bhoi <mayurbhoi200@gmail.com>
2022-12-21 12:56:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 const DB = process.env.DB;
 
